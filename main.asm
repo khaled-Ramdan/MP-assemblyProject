56494e37c7730720838b8b23b962ae9348f21bcf
Mariam ElMonyer <64711347+mariam-elmonyer@users.noreply.github.com>
2022-12-12 10:24:25 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -23,6 +23,50 @@ score dw 02h
 
 ;;-------------- PROGRAM STARTS-------------------------
 
+maindata:
+   
+    mov dx, offset msg1
+    mov ah, 9
+    int 21h
+    
+    
+    mov ah, 1
+    int 21h
+    
+    cmp al,31h
+    jz colorBlue 
+     
+    cmp al,32h
+    jz colorRed   
+    
+    cmp al,33h
+    jz colorMagenta 
+    
+    cmp al,34h
+    jz colorCyan     
+    
+    cmp al,35h
+    jz colorGreen
+    
+    colorBlue:   
+    mov player1,0c01h   
+    jmp plot
+    
+    colorRed:  
+    mov player1,0c04h   
+    jmp plot
+    
+    colorMagenta:  
+    mov player1,0c05h 
+    jmp plot
+    
+    colorCyan:
+    mov player1,0c0bh
+    jmp plot 
+    
+    colorGreen:
+    mov player1,0c02h
+    jmp plot 
 
 ;;-------------------------SET BOUNDRY/PLAY AREA -------
     plot:     
