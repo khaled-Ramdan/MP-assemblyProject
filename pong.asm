57dc1a595e6fc91f1c16b370291cc4143b1e8dda
Khaled <66100565+khaled-Ramdan@users.noreply.github.com>
2022-12-17 14:06:07 +0200

    Update pong.asm

--- pong.asm
+++ pong.asm
@@ -5,7 +5,7 @@ STACK ENDS
 DATA SEGMENT PARA 'DATA'
 	BALL_X DW 0Ah; x position (coloum) of the ball
 	BALL_Y DW 64h; y position (line) of the ball 
-	BALL_SIZE DW 64h;size of the ball (how many pixels does the ball have in width and height)
+	BALL_SIZE DW 06h;size of the ball (how many pixels does the ball have in width and height)
 	
 	
 	
@@ -42,30 +42,31 @@ CODE SEGMENT PARA 'CODE'
 	MAIN ENDP
 	
 	
-	DRAW_BALL PROC NEAR ;near so main procedure can call this procedure
-
-		MOV CX,BALL_X;set the initial coloum (x)
-		MOV DX,BALL_Y; set the intial line(Y)
-
+	DRAW_BALL PROC NEAR                  
+		
+		MOV CX,BALL_X                    ;set the initial column (X)
+		MOV DX,BALL_Y                    ;set the initial line (Y)
+		
 		DRAW_BALL_HORIZONTAL:
-			MOV AH,0Ch;set the configuration writing a pixel 
-			MOV AL,0Fh;choose white as color of pixel 
-			MOV BH,00h;set the page number 
-			INT 10h;execute the configuration
+			MOV AH,0Ch                   ;set the configuration to writing a pixel
+			MOV AL,0Fh 					 ;choose white as color
+			MOV BH,00h 					 ;set the page number 
+			INT 10h    					 ;execute the configuration
 			
-			INC CX ; cx++
-			MOV AX,CX;  CX - BALL_X > BALL_SIZE (True => we go to the next line, False => we continue to the next coloum )
+			INC CX     					 ;CX = CX + 1
+			MOV AX,CX          	  		 ;CX - BALL_X > BALL_SIZE (Y -> We go to the next line,N -> We continue to the next column
 			SUB AX,BALL_X
 			CMP AX,BALL_SIZE
-			JNC DRAW_BALL_HORIZONTAL
+			JNG DRAW_BALL_HORIZONTAL
 			
-			MOV CX,BALL_X; the cx register goes back to the initial coloum
-			INC DX		 ; we advance one line 
+			MOV CX,BALL_X 				 ;the CX register goes back to the initial column
+			INC DX       				 ;we advance one line
 			
-			MOV AX,DX 	; Dx - BALL_SIZE (True => we exit this procedure , False => we continue to the next line)
+			MOV AX,DX             		 ;DX - BALL_Y > BALL_SIZE (Y -> we exit this procedure,N -> we continue to the next line
 			SUB AX,BALL_Y
 			CMP AX,BALL_SIZE
-			JNC DRAW_BALL_HORIZONTAL
+			JNG DRAW_BALL_HORIZONTAL
+		
 		RET
 	DRAW_BALL ENDP
 CODE ENDS
